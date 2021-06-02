9925e008d266155fce8800722e73e0a624f986c7
Tan Rae-Win <raewintan@gmail.com>
2021-06-02 18:24:20 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-mouse-textbox-demo';
+  strValue:string = "";
+  down:number = 0;
+  zindex:number = 0;
+
+  @HostListener('document:mousedown',['$event'])
+  mousedown(evt){
+    this.down++;
+  }
+
+  @HostListener('document:mouseup',['$event'])
+  mouseup(evt){
+    this.down--;
+  }
+
+  @HostListener('document:mousemove',['$event'])
+  mousemove(evt){
+    this.strValue = " x : "+ evt.pageX+"y : "+evt.pageY;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+import {AngularMouseTextboxModule} from "angular-mouse-textbox";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+     AngularMouseTextboxModule,
+     FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
